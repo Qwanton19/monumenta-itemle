a4296cbfe458a8259abbf172ddaf07b2f7a075e3
Alecaboo <alec.malcangio@icloud.com>
2025-05-30 14:34:14 -0700

    Fix for weird charm positive/negatives

--- utils/items/charmFormatter.js
+++ utils/items/charmFormatter.js
@@ -13,18 +13,24 @@ class CharmFormatter {
             .filter(part => (part != "m" && part != "p" && part != "bow" && part != "tool"))
             .map(part => part[0].toUpperCase() + part.substring(1))
             .join(" ");
-        
+
         humanStr = `${(value > 0) ? "+" : ""}${value}${(humanStr.includes(" Percent")) ? "%" : ""} ${humanStr.replace(" Percent", "").replace(" Base", "").replace(" Flat", "")}`;
 
         return humanStr;
     }
 
     static statStyle(stat, value) {
-        return ((stat.includes("cooldown")  && !stat.includes("reduction")) || stat.includes("requirement")
-            || stat.includes("price") || stat.includes("self_damage") || stat.includes("delay")) ?
-            (value < 0) ? "positiveCharm" : "negativeCharm" :
-            (value < 0) ? "negativeCharm" : "positiveCharm";
-    }
+    return (
+        (stat.includes("cooldown") && (!stat.includes("reduction") && (!stat.includes("cap_flat"))))
+        || stat.includes("price")
+        || stat.includes("threshold")
+        || stat.includes("stacks_needed_for_activation_flat") // ok that one's a little gross I admit
+        || stat.includes("self_damage")
+        || stat.includes("delay")
+    ) ?
+        (value < 0) ? "positiveCharm" : "negativeCharm" :
+        (value < 0) ? "negativeCharm" : "positiveCharm";
+}
 
     static formatCharm(charm) {
         let formattedStats = [];
