b7e8845b33bf5265ea2fbea381afebf690d98675
LucyChroma <lucychroma@discord>
2025-06-22 20:31:13 -0400

    Testing link preview fix

--- pages/builder.js
+++ pages/builder.js
@@ -32,8 +32,8 @@ function getLinkPreviewDescription(build, itemData) {
 }
 
 function getBuildName(build, builderHeaderText) {
-    if(builderHeaderText === "Monumenta Builder") return "";
-    if(builderHeaderText !== undefined) return builderHeaderText + " - ";
+    if(parentLoaded && builderHeaderText === "Monumenta Builder") return "";
+    if(parentLoaded && builderHeaderText !== undefined) return builderHeaderText + " - ";
     // console.log("getting build name from ",build)
     if (!build) return "";
     const buildParts = build[0].split("&");
